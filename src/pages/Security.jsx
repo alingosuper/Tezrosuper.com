1cef8e81d8d3813dba534e35b17dfa83da23c514
alingosuper <alingosuper@gmail.com>
2026-03-09 05:30:30 +0500

    Create Security.jsx

---
+++ src/pages/Security.jsx
@@ -0,0 +1,23 @@
+function Security(){
+
+return(
+
+<div>
+
+<h1>Tezro Security</h1>
+
+<p>
+
+Tezro uses encrypted authentication,
+secure API gateway,
+and fraud protection.
+
+</p>
+
+</div>
+
+)
+
+}
+
+export default Security
